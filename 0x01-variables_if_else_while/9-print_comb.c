fae4baeee92ce746fd5fecd771b36c4127accabe
DeveloperVictor1 <developervictor1@gmail.com>
2023-03-17 08:38:26 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+putchar(j + '0');
+if (i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
+
