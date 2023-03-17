35e56ea491af63a6ca90db53984a77e8910a48e3
DeveloperVictor1 <developervictor1@gmail.com>
2023-03-17 14:05:59 -0700

    101-print_comb4.ci

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,16 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main (void)
 {
 int i, j, k;
-for (i = 0; i < 10; i++) 
+for (i = 0; i < 8; i++) 
 {
-for (j = i + 1; j < 10; j++) 
+for (j = i + 1; j < 9; j++) 
 {
 for (k = j + 1; k < 10; k++) 
-{
-if (i != j && i != k && j != k) 
 {
 putchar(i + '0');
 putchar(j + '0');
@@ -22,11 +20,10 @@ if (i < 7 || j < 8 || k < 9)
 {
 putchar(',');
 putchar(' ');
-}                
-}           
 }
 }
 }
-putchar ('\n');
+}
+putchar('\n');
 return (0);
 }
