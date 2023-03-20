277728e88dee31424a5c680616e9317d9863ce14
DeveloperVictor1 <developervictor1@gmail.com>
2023-03-20 13:00:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <unistd.h> 
- /** 
-  * main - write to stdout 
-  * Return: Always 0 
-  */ 
- int main(void) 
- { 
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-
-return (1); 
- }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: Prints the last digit of a randomly generated number
+ * Return: Always 0 (success)
+ */
+int main (void)
+{ 
+write(2,"and that piece of art is useful - Doa Korpar, 2015-10-19\n",58); 
+return (1);  
+} 
+ 
