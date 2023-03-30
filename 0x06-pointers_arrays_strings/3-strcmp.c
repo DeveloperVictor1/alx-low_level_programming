00d97747e792a0a6f90b8f88a878ab3e546ac114
DeveloperVictor1 <developervictor1@gmail.com>
2023-03-30 04:16:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
-*_strcmp - compare string values
-*@s1: input value
-*@s2: input value
+* _strcmp - compare string values
+* @s1: input value
+* @s2: input value
 *
-*Return: s1[i] - s2[i]
+* Return: s1[i] - s2[i]
 */
 int _strcmp(char *s1, char *s2)
 {
 int i;
 i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
