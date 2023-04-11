ecb09c25acc91b5c266c6b54af9cadcbb1f69fff
DeveloperVictor1 <developervictor1@gmail.com>
2023-04-11 05:32:53 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
